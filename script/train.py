48dd714853110e4e6084193f4536ead994c6ac4c
Ananda Ravi Kuntadi <anandaravik@gmail.com>
2024-11-13 09:10:26 +0700

    "Configure DVC with Supabase S3 credentials, update MLOps workflow to use DVC, and modify training script"

--- script/train.py
+++ script/train.py
@@ -18,7 +18,7 @@ val_dir = os.path.abspath(os.path.join(os.getcwd(), "../dataset/val"))
 test_dir = os.path.abspath(os.path.join(os.getcwd(), "../dataset/test"))
 
 # Hyperparameters
-num_epochs = 25
+num_epochs = 1
 batch_size = 4
 num_classes = 4
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
